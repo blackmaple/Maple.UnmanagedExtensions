68e28265e2bb16027695639cc264e9ffd73cc8f3
红色枫叶 <BlackMaple@live.cn>
2026-02-20 20:24:40 +0800

    添加项目文件。

---
+++ Maple.UnmanagedExtensions/GCNormalSelf.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Maple.UnmanagedExtensions
+{
+    public class GCNormalSelf<T> : IDisposable where T : GCNormalSelf<T>
+    {
+        GCHandle<GCNormalSelf<T>> Handle { get; }
+        public nint HandlePointer => GCHandle<GCNormalSelf<T>>.ToIntPtr(Handle);
+
+        public GCNormalSelf()
+        {
+            Handle = new GCHandle<GCNormalSelf<T>>(this);
+        }
+
+        public void Dispose()
+        {
+            this.Handle.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Maple.UnmanagedExtensions/GCPinnedSelf.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Maple.UnmanagedExtensions
+{
+    public class GCPinnedSelf<T> : IDisposable where T : GCPinnedSelf<T>
+    {
+        PinnedGCHandle<GCPinnedSelf<T>> Handle { get; }
+        public nint HandlePointer => PinnedGCHandle<GCPinnedSelf<T>>.ToIntPtr(Handle);
+
+        public GCPinnedSelf()
+        {
+            Handle = new PinnedGCHandle<GCPinnedSelf<T>>(this);
+        }
+
+        public void Dispose()
+        {
+            this.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
