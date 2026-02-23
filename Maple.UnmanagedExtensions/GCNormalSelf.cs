6619d052585e96defa432da9912ec30501d90eec
红色枫叶 <BlackMaple@live.cn>
2026-02-23 12:34:25 +0800

    1

--- Maple.UnmanagedExtensions/GCNormalSelf.cs
+++ Maple.UnmanagedExtensions/GCNormalSelf.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Maple.UnmanagedExtensions
 {
@@ -10,6 +12,20 @@ namespace Maple.UnmanagedExtensions
         public GCNormalSelf()
         {
             this.Obj = GCHandle.Alloc(this, GCHandleType.Normal);
+        }
+        public static bool TryGet<T>(nint ptr, [MaybeNullWhen(false)] out T obj) where T:GCNormalSelf
+        {
+            Unsafe.SkipInit(out obj);
+            if (ptr != nint.Zero)
+            {
+                var handle = GCHandle.FromIntPtr(ptr);
+                if (handle.IsAllocated && handle.Target is T item)
+                {
+                    obj = item;
+                    return true;
+                }
+            }
+            return false;
         }
 
 
--- Maple.UnmanagedExtensions/GCPinnedSelf.cs
+++ Maple.UnmanagedExtensions/GCPinnedSelf.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Maple.UnmanagedExtensions
 {
@@ -12,6 +14,31 @@ namespace Maple.UnmanagedExtensions
             this.Obj = GCHandle.Alloc(this, GCHandleType.Pinned);
         }
 
+        public static bool TryGetFromHandle<T>(nint ptr, [MaybeNullWhen(false)] out T obj) where T : GCPinnedSelf
+        {
+            Unsafe.SkipInit(out obj);
+            if (ptr != nint.Zero)
+            {
+                var handle = GCHandle.FromIntPtr(ptr);
+                if (handle.IsAllocated && handle.Target is T item)
+                {
+                    obj = item;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool TrtGetFromPointer<T>(nint ptr, [MaybeNullWhen(false)] out T obj) where T : GCPinnedSelf
+        {
+            Unsafe.SkipInit(out obj);
+            if (ptr != nint.Zero)
+            {
+                obj = Unsafe.As<nint, T>(ref ptr);
+                return true;
+            }
+            return false;
+        }
 
         public void Dispose()
         {
