9c6698ec61d5b87cfb27d19a132805f05613a476
红色枫叶 <BlackMaple@live.cn>
2026-03-08 21:04:42 +0800

    .add unsafeout

---
+++ Maple.UnmanagedExtensions/UnsafeOut.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Maple.UnmanagedExtensions
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly unsafe struct UnsafeOut<T>(scoped ref T data) where T : unmanaged
+    {
+        [MarshalAs(UnmanagedType.SysInt)]
+        readonly nint _ptr = new(Unsafe.AsPointer(ref data));
+
+        //public ref T Raw => ref Unsafe.AsRef<T>(_ptr.ToPointer());
+
+        public static UnsafeOut<T> FromOut(out T data)
+        {
+            Unsafe.SkipInit(out data);
+            return new UnsafeOut<T>(ref data);
+        }
+
+
+        public static implicit operator nint(UnsafeOut<T> data)=> data._ptr;
+    }
+}
